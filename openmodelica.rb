511d0115b88ce0540612101e3d2fd84a40203fba
Guilheme Brondani Torri <guitorri@gmail.com>
2014-03-11 00:01:39 +0100

    back to 1.9.0

--- openmodelica.rb
+++ openmodelica.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Openmodelica < Formula
   homepage 'http://www.openmodelica.org/'
-  url 'https://build.openmodelica.org/apt/pool/contrib/openmodelica_19489.orig.tar.gz'
+  url 'https://build.openmodelica.org/apt/pool/contrib/openmodelica_17628.orig.tar.gz'
   sha1 'cb31e8cb1e7b6b1c1e34491dc526bffebf234c6a'
   version '1.9.0'
 
@@ -14,7 +14,7 @@ class Openmodelica < Formula
   depends_on 'libtool' => :build
   depends_on 'bison' => :build
   depends_on 'flex' => :build
-  depends_on 'rml-mmc' => :build
+  depends_on 'guitorri/tap/rml-mmc' => :build
   depends_on 'readline' => :build
   depends_on 'sundials' => :build
   depends_on 'sqlite' => [:build, 'universal']
@@ -25,27 +25,37 @@ class Openmodelica < Formula
   depends_on 'lp_solve' => [:build, 'python']
   depends_on 'omniorb' => :build
 
-  # skip unwanted tools, configure Qt framework on OMPlot
-  def patches
-    DATA
-  end
+  depends_on :fortran  # for ipopt
 
-  fails_with :clang do
-    build 500
-    cause %q[error: reference to 'lock_guard' is ambiguous.]
-   end
+
+  def patches
+    [
+    # 1) select Qt clients to build, skip omoptim omoptimbasis
+    "https://gist.githubusercontent.com/guitorri/9475078/raw/faeea3d6abcaa867cc6563340ae84693ba5e6952/openmodelica_select_Qt_tools.patch",
+    # 2) pass Qwt as framework for OMPlot
+    "https://gist.githubusercontent.com/guitorri/9475193/raw/8bc871dfceefb8ddfb104ba7efb8df66777a4426/openmodlica_omplot_qwt_framework.patch",
+    # 3) pass Qwt as framework for OMEdit
+    "https://gist.githubusercontent.com/guitorri/9475172/raw/2f658d29c6147f0af0f10863d821c6be0a83aa37/openmodlica_omedit_qwt_framework.patch",
+    # 4) fix std:: ref and lock name conflicts with std:ref and std::lock
+    "https://gist.githubusercontent.com/guitorri/9475214/raw/703cdc6a07292917e68ecc6d0e32406b184e91d8/openmodelica_corba_name_conflict.patch"
+    ]
+  end
 
 
   def install
     system "autoconf"
     system "./configure", "--prefix=#{prefix}",
-                          #"--without-rml",
                           "--with-omniORB=#{Formula.factory('omniorb').opt_prefix}",
                           "--with-qwt=#{Formula.factory('qwt').opt_prefix}",
                           "--without-paradiseo",
                           "--with-lapack=-framework Accelerate", # avoid the included OpenBlas
                           "--enable-modelica3d=no",
                           "--enable-omnotebook=no"
+
+    if ENV.compiler == :clang
+      ENV.append 'CXXFLAGS', '-stdlib=libc++'
+    end
+
     system "make"
     system "make", "install"
   end
@@ -54,33 +64,4 @@ class Openmodelica < Formula
     system "omc", "++v"
   end
 end
-
-__END__
-diff --git a/Makefile.in b/Makefile.in
-index 9ea401d..c12b406 100644
---- a/Makefile.in
-+++ b/Makefile.in
-@@ -78,7 +78,7 @@ omoptimbasis: mkbuilddirs
- 	$(MAKE) -C OMOptimBasis/build -f Makefile.unix
- endif
- 
--qtclients: @OMNOTEBOOK@ omshell omedit omplot omoptim omoptimbasis
-+qtclients: @OMNOTEBOOK@ omplot # omshell omedit omoptim omoptimbasis
- 
- qtclean: qtclean-common
- 	$(MAKE) -C OMShell/OMShellGUI -f Makefile.unix clean
-diff --git a/OMPlot/OMPlotGUI/OMPlotGUI.config.in b/OMPlot/OMPlotGUI/OMPlotGUI.config.in
-index c9f2c57..60d6196 100644
---- a/OMPlot/OMPlotGUI/OMPlotGUI.config.in
-+++ b/OMPlot/OMPlotGUI/OMPlotGUI.config.in
-@@ -2,5 +2,7 @@ QMAKE_CC  = @CC@
- QMAKE_CXX = @CXX@
- QMAKE_LINK = @CXX@
- 
--LIBS += -lqwt@with_qwt_suffix@
--INCLUDEPATH += @with_qwt@
-+QMAKEFEATURES += @with_qwt@/features
-+CONFIG += qwt
-+LIBS += -F@with_qwt@/lib/ -framework qwt
-+INCLUDEPATH += @with_qwt@/lib/qwt.framework/Headers
 
--- rml-mmc.rb
+++ rml-mmc.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class RmlMmc < Formula
   homepage 'http://www.ida.liu.se/labs/pelab/rml'
-  #url 'http://build.openmodelica.org/apt/pool/contrib/rml-mmc_260.orig.tar.gz'
-  #version '2.6.0'
-  #sha1 '22acb73e9d5e0a52c853a8cf73cf10d02d749d36'
-  url 'http://build.openmodelica.org/apt/pool/contrib/rml-mmc_271.orig.tar.gz'
-  version '2.7.1'
-  sha1 '2805a46d9c9abae5c7f007b26faf4927b88bd541'
+  url 'http://build.openmodelica.org/apt/pool/contrib/rml-mmc_260.orig.tar.gz'
+  version '2.6.0'
+  sha1 '22acb73e9d5e0a52c853a8cf73cf10d02d749d36'
+  #url 'http://build.openmodelica.org/apt/pool/contrib/rml-mmc_271.orig.tar.gz'
+  #version '2.7.1'
+  #sha1 '2805a46d9c9abae5c7f007b26faf4927b88bd541'
 
 
   # Attention, has a self-signed certificate and svn will prompt you, so
@@ -16,12 +16,19 @@ class RmlMmc < Formula
 
   depends_on 'smlnj'
 
+  def patches
+    # fixes non-void return value
+    # fix undefined inline, see:
+      #http://stackoverflow.com/questions/10243018/inline-function-undefined-symbols-error
+    DATA
+  end
+
   def install
     ENV.j1
     ENV['SMLNJ_HOME'] = Formula.factory("smlnj").prefix/'SMLNJ_HOME'
 
-    system "./configure --prefix=#{prefix} --disable-debug"
-    system "make CC=clang CPFLAGS=-fno-omit-frame-pointer"
+    system "./configure --prefix=#{prefix}"
+    system "make CPFLAGS=-fno-omit-frame-pointer"
     system "make install"
   end
 
@@ -29,3 +36,57 @@ class RmlMmc < Formula
     system "#{bin}/rml", "-v"
   end
 end
+
+__END__
+diff --git a/runtime/debugging/debug.c b/runtime/debugging/debug.c
+index 8d855da..da7a950 100644
+--- a/runtime/debugging/debug.c
++++ b/runtime/debugging/debug.c
+@@ -603,7 +603,7 @@ char* rmldb_get_relation_type()
+ 		if (strcmp(name, tmpr->name) == 0)
+ 		{
+ 			type = tmpr->type_db->type;
+-			if (!type) { fprintf(stdout,"()"); return; } 
++			if (!type) { fprintf(stdout,"()"); return ""; } 
+ 			switch (type->kind)
+ 			{
+ 			case RMLDB_eLISTty:  
+diff --git a/runtime/common/rml-core.h b/runtime/common/rml-core.h
+index 3a21095..f2fee5c 100644
+--- a/runtime/common/rml-core.h
++++ b/runtime/common/rml-core.h
+@@ -337,7 +337,7 @@ extern rml_sint_t rml_prim_stringeq(void*, rml_uint_t, const char*);
+ #ifdef _MSC_VER
+ extern void *rml_prim_equal(void*, void*);
+ #else
+-inline void *rml_prim_equal(void*, void*);
++extern inline void *rml_prim_equal(void*, void*);
+ #endif
+ extern void  rml_prim_unwind_(void**);
+ 
+diff --git a/runtime/common/value.c b/runtime/common/value.c
+index ffc7082..0402d3a 100644
+--- a/runtime/common/value.c
++++ b/runtime/common/value.c
+@@ -52,7 +52,7 @@ RML_END_LABEL
+ #define inline inline
+ #endif
+ 
+-inline void *rml_prim_equal(void *p, void *q)
++extern inline void *rml_prim_equal(void *p, void *q)
+ {
+   tail_recur:
+     /* INV: ISIMM(p) <==> ISIMM(q) */
+diff --git a/runtime/common/value.c b/runtime/common/value.c
+index 0402d3a..4dd9125 100644
+--- a/runtime/common/value.c
++++ b/runtime/common/value.c
+@@ -105,7 +105,7 @@ extern inline void *rml_prim_equal(void *p, void *q)
+ }
+ 
+ 
+-inline long long unsigned rml_prim_hash(void *p)
++static inline long long unsigned rml_prim_hash(void *p)
+ {
+   long long unsigned hash = 0;
+   void **pp = NULL;
