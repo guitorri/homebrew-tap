80a619ad6ea191b95b2d2078304f37d7fb2908a4
Guilherme Brondani Torri <guitorri@gmail.com>
2014-08-17 12:42:21 +0200

    adms 2.3.3

--- adms.rb
+++ adms.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Adms < Formula
   homepage 'https://github.com/Qucs/ADMS'
-  url 'http://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.2.tar.gz'
-  sha1 'ada4bcc90903a8b8bd698a888ee432a540fd42f5'
+  url 'http://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.3.tar.gz'
+  sha1 'a1cbe5a159c46c90f3d8f9c6ea1fb42dbb3044be'
 
   head do
     url 'https://github.com/Qucs/ADMS.git'
@@ -19,14 +19,11 @@ class Adms < Formula
   depends_on 'libtool' => :build
 
   def install
-    ENV.j1 # does not build in parallel
-
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
-
   end
 
   test do
