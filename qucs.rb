f4865c0a8e9cd80cc97844ef99789c71851f9e5c
Guilherme Brondani Torri <guitorri@gmail.com>
2014-08-31 14:30:10 +0200

    qucs 0.0.18

--- qucs.rb
+++ qucs.rb
@@ -2,8 +2,17 @@ require 'formula'
 
 class Qucs < Formula
   homepage 'http://sourceforge.net/projects/qucs/'
-  url 'http://sourceforge.net/projects/qucs/files/qucs/0.0.17/qucs-0.0.17.tar.gz'
-  sha1 '756f993aa27f291faae92303d459f372940590bc'
+
+  stable do
+    url 'http://sourceforge.net/projects/qucs/files/qucs/0.0.18/qucs-0.0.18.tar.gz'
+    sha1 'e13580c12fa4f9d7a09f6a2b412bd772362e44ac'
+  end
+
+  head do
+    url  'https://github.com/Qucs/qucs.git', :branch => 'master'
+    depends_on 'asco' => :build
+    depends_on 'libtool' => :build
+  end
 
   depends_on 'pkg-config' => :build
   depends_on 'autoconf' => :build
@@ -12,38 +21,38 @@ class Qucs < Formula
   depends_on 'flex' => :build
   depends_on 'qt' => [:build, "with-qt3support"]
 
-  depends_on "apple-gcc42" if MacOS.version >= :mavericks
-
+  # use ADMS and ASCO formulae, disable configure/build of shipped packages
+  depends_on 'adms' => :recomended
+  depends_on 'asco' => :recomended
   #depends_on 'octave' => :optional
-
-  #./qucs-0.0.17.mountain_lion.bottle.tar.gz
-  #bottle do
-  #  sha1 '130e6ad1d76b554a8a22cf46e025ce450e551048' => :mountain_lion
-  #end
-
-  def patches
-    # configure asco as Unix
-    DATA
-  end
 
   def install
 
-    if ENV.compiler == :clang
-      opoo <<-EOS.undent
-        Qucs 0.0.17 will fail with latest Clang.
-        Please use:
-          brew install qucs --cc=gcc-4.2
-      EOS
-    end
+    if build.head?
 
-    # update asco configure script after patching
-    cd 'asco' do
-    system "autoconf"
-    end
+      cd 'qucs' do
+        system "sh", "autogen.sh"
+        system "./configure", "--enable-maintainer-mode", "--prefix=#{prefix}"
+        system "make", "install"
+      end
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+      ENV.j1 # head does not build in parallel, race on ADMS? issue with newer bison fix adms
+
+      cd 'qucs-core' do
+        system "sh", "bootstrap.sh"
+        system "./configure", "--enable-maintainer-mode", "--prefix=#{prefix}"
+        system "make", "install"
+      end
+
+    # stable
+    else
+      system "./configure", "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-asco", # use formula
+                            "--disable-adms", # use formula
+                            "--prefix=#{prefix}"
+      system "make", "install"
+    end # if stable
   end
 
   def caveats; <<-EOS.undent
@@ -58,24 +67,4 @@ class Qucs < Formula
   end
 
 end
-
-__END__
-diff --git a/asco/configure.ac b/asco/configure.ac
-index 6d362b0..6300469 100644
---- a/asco/configure.ac
-+++ b/asco/configure.ac
-@@ -53,6 +53,13 @@ if test "x$GCC" = xyes; then
-   fi
- fi
- 
-+dnl Force the Unix definition
-+case $host_os in
-+  *darwin*)
-+  CFLAGS="$CFLAGS -DUNIX -I/usr/include/malloc"
-+  ;;
-+esac
-+
- dnl Check for MP-ICC.
- AC_PATH_PROG(CC_MPI, mpicc, :)
- AM_CONDITIONAL(MPI, test "$CC_MPI" != ":")
 
