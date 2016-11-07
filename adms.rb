eb1626b361dcbfb6ad8ca8a25d27fb9e9fbd544a
Guilherme Brondani Torri <guitorri@gmail.com>
2016-11-07 22:45:42 +0100

    Revert "adms-2.3.5"
    This reverts commit 7a26b1f43e45c269a22610bc263adb5bf25cdab8.
    
    adms-2.3.5 has an issue on 64bit, see
    https://github.com/Qucs/ADMS/issues/51

--- adms.rb
+++ adms.rb
@@ -2,13 +2,14 @@ require 'formula'
 
 class Adms < Formula
   homepage 'https://github.com/Qucs/ADMS'
-  url 'http://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.5.tar.gz'
-  sha256 'c8472b201f3601952b20a70dcd5a784956c1109da2d9f6452271d0337e8d8634'
+  url 'http://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.4.tar.gz'
+  sha256 '63498c24c3064ae86eae135ac7324e532bc8bb772eca5208204814df8f27931a'
 
   head do
     url 'https://github.com/Qucs/ADMS.git'
     depends_on 'cmake' => :build
     depends_on 'XML::LibXML' => :perl
+    depends_on 'GD' => :perl
   end
 
   depends_on 'automake'
