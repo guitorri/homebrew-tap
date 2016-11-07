7a26b1f43e45c269a22610bc263adb5bf25cdab8
Guilherme Brondani Torri <guitorri@gmail.com>
2016-11-07 21:39:55 +0100

    adms-2.3.5

--- adms.rb
+++ adms.rb
@@ -2,14 +2,13 @@ require 'formula'
 
 class Adms < Formula
   homepage 'https://github.com/Qucs/ADMS'
-  url 'http://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.4.tar.gz'
-  sha256 '63498c24c3064ae86eae135ac7324e532bc8bb772eca5208204814df8f27931a'
+  url 'http://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.5.tar.gz'
+  sha256 'c8472b201f3601952b20a70dcd5a784956c1109da2d9f6452271d0337e8d8634'
 
   head do
     url 'https://github.com/Qucs/ADMS.git'
     depends_on 'cmake' => :build
     depends_on 'XML::LibXML' => :perl
-    depends_on 'GD' => :perl
   end
 
   depends_on 'automake'
